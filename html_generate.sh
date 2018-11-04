93ef700a977380836b68171dc1a310590e1f88b3
yamarten <yamarten@mail.uec.jp>
2018-11-04 19:04:51 +0900

    html生成スクリプトのバグを修正

--- html_generate.sh
+++ html_generate.sh
@@ -4,12 +4,12 @@ if [ -e doc/sphinx/build/doctrees ]; then
 else
   :
 fi
-if [ -e doc/sphinx/locales/ja/LC_MESSAGES/ ]
+if [ -e doc/sphinx/locales/ja/LC_MESSAGES/ ]; then
   mkdir -p doc/sphinx/locales/ja/LC_MESSAGES/
 else
   :
 fi
 cp -f ../target/*.po doc/sphinx/locales/ja/LC_MESSAGES/
 make refman-html SPHINXOPTS='-D language="ja"'
-cp -rf doc/sphinx/_build/html/ ..
+cp -rf doc/sphinx/_build/html/* ../html/refman
 cd ..
